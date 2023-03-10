5dd9fed3c1e7f325b716e737ba6727f433224d68
rezah11 <46706569+rezah11@users.noreply.github.com>
2023-03-10 22:35:03 +0330

    feat(authController): simple passport login

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -14,15 +14,26 @@ class authController extends Controller
             'password' => $request->password
         ]);
 
-        if(auth()->check()){
-            return auth()->user()->generateToken();
+        //generate token manual
+//        if (auth()->check()) {
+//            return auth()->user()->generateToken();
+//        }
+//        return response(['error' => 'اطلاعات کاربری را اشتباه وارد کرده اید'], 401);
+//    }
+
+//        generate token passport
+        if (auth()->check()) {
+            $token = auth()->user()->createToken('test');
+//             dd($token->toJson());
+            return response(['token'=> $token->accessToken],200);
         }
-        return response(['error'=>'اطلاعات کاربری را اشتباه وارد کرده اید'],401);
+        return response(['error' => 'اطلاعات کاربری را اشتباه وارد کرده اید'], 401);
     }
+
 
     public function logoutApi()
     {
-        $user=auth()->guard('api')->user();
+        $user = auth()->guard('api')->user();
         $user->logout();
         return $user;
     }
